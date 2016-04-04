430432b5c3c06a1f29b18e77514a79b6e1ea8e07
moses-aronov <moses@aronov.me>
2016-04-04 13:38:13 -0400

    Added collection view cell and class

---
+++ Pokedex/PokemonCollectionViewCell.swift
@@ -0,0 +1,25 @@
+//
+//  PokemonCollectionViewCell.swift
+//  Pokedex
+//
+//  Created by Moses Aronov on 4/4/16.
+//  Copyright © 2016 Moses Aronov. All rights reserved.
+//
+
+import UIKit
+
+class PokemonCollectionViewCell: UICollectionViewCell {
+    //MARK: IBOutlets
+    @IBOutlet weak var thumbnailImage: UIImageView!
+    @IBOutlet weak var thumbnailLabel: UILabel!
+    
+    //MARK: Variables
+    var pokemon: Pokemon!
+    
+    //MARK: Functions
+    func configureCell(pokemon: Pokemon){
+        self.pokemon = pokemon
+        thumbnailLabel.text = self.pokemon.name.capitalizedString
+        thumbnailImage.image = UIImage(named: "\(self.pokemon.pokedexId)")
+    }
+}
